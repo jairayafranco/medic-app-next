b6675c8ef91d598b413960e3ec4223f94f258cc5
Jair Aya <jair.aya@uniminuto.edu.co>
2022-10-29 00:43:31 -0500

    validacion de layout segun ruta

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,20 @@
 import Dashboard from "../layout/Dashboard"
+import Head from "next/head"
+import { useRouter } from "next/router";
 
 export default function MyApp({ Component, pageProps }) {
+    const router = useRouter();
+
     return (
-        <Dashboard>
-            <Component {...pageProps} />
-        </Dashboard>
+        <>
+            <Head>
+                <title>MedicApp</title>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+            {
+                ['/login', '/_error'].includes(router.pathname) ? <Component {...pageProps} /> : <Dashboard><Component {...pageProps} /></Dashboard>
+            }
+        </>
     )
 }
