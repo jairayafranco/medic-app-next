3f75e6e4ee2f89c80460b2931de51182c9eb8608
Jair Aya <jair.aya@uniminuto.edu.co>
2022-10-30 14:59:30 -0500

    ajustes al middleware

--- middleware.js
+++ middleware.js
@@ -3,22 +3,38 @@ import { jwtVerify } from "jose";
 
 export async function middleware(request) {
     const token = request.cookies.get('token');
+    const url = request.nextUrl.pathname;
 
-    if (!token) {
+    if (!token && url !== '/login') {
         return NextResponse.redirect(new URL('/login', request.url));
     }
 
-    try {
-        await jwtVerify(token, new TextEncoder().encode(process.env.SECRET));
-        if(request.nextUrl.pathname === '/') {
-            return NextResponse.redirect(new URL('/panel', request.url));
-        }
-        return NextResponse.next();
-    } catch (err) {
+    const result = await validateToken(token);
+
+    if (result && url === '/') {
+        return NextResponse.redirect(new URL('/panel', request.url));
+    }
+
+    if (!result && url.startsWith('/panel')) {
         return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    if (result && url === '/login') {
+        return NextResponse.redirect(new URL('/panel', request.url));
+    }
+
+    // return NextResponse.next();
+
+    async function validateToken(token) {
+        try {
+            await jwtVerify(token, new TextEncoder().encode(process.env.SECRET));
+            return true;
+        } catch (err) {
+            return false;
+        }
     }
 }
 
 export const config = {
-    matcher: ['/panel/:path*', '/'],
+    matcher: ['/panel/:path*', '/', '/login'],
 }
