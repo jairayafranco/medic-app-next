b844571e4c584859c6db088400f6a9a506dca6fc
Jair Aya <jair.aya@uniminuto.edu.co>
2022-10-29 21:36:46 -0500

    Se agrego el cambio de tema y spinner al cargar las paginas

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,65 @@
 import Dashboard from "../layout/Dashboard"
 import Head from "next/head"
 import { useRouter } from "next/router";
+import { useEffect, useState, useMemo } from "react";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Spinner from "../components/Spinner";
 
 export default function MyApp({ Component, pageProps }) {
     const router = useRouter();
+    const DOMLoaded = useState(false); // This is a hack to prevent the theme from flashing on page load. It's not a perfect solution, but it's better than nothing.
+    const [loading, setLoading] = useState(false);
+
+    const [mode, setMode] = useState('light');
+    const colorMode = useMemo(
+        () => ({
+            toggleColorMode: () => {
+                setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+            },
+        }),
+        [],
+    );
+
+    const themeMode = useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode,
+                },
+            }),
+        [mode],
+    );
+
+    useEffect(() => {
+        DOMLoaded[1](true);
+        const getTheme = localStorage.getItem('MUIThemeMode');
+        if (getTheme) {
+            setMode(getTheme);
+        }
+
+        router.events.on('routeChangeStart', () => setLoading(true));
+        router.events.on('routeChangeComplete', () => setLoading(false));
+        router.events.on('routeChangeError', () => setLoading(false));
+    }, [])
 
     return (
-        <>
-            <Head>
-                <title>MedicApp</title>
-                <meta charset="UTF-8" />
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            </Head>
-            {
-                ['/login', '/_error'].includes(router.pathname) ? <Component {...pageProps} /> : <Dashboard><Component {...pageProps} /></Dashboard>
-            }
-        </>
+        DOMLoaded[0] && (
+            <>
+                <Head>
+                    <title>MedicApp</title>
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                </Head>
+                <ThemeProvider theme={themeMode}>
+                    {
+                        ['/login', '/_error'].includes(router.pathname)
+                            ? <Component {...pageProps} />
+                            :
+                            <Dashboard changeTheme={colorMode} theme={themeMode}>
+                                {loading ? <Spinner minHeight="80vh" /> : <Component {...pageProps} />}
+                            </Dashboard>
+                    }
+                </ThemeProvider>
+            </>
+        )
     )
 }
