67b551f43026bc1e300683b483a2f06edcf0eb6d
Jair Aya <jair.aya@uniminuto.edu.co>
2022-10-30 21:20:21 -0500

    Agregados estilos globales

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
     const router = useRouter();
