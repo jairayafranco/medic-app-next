dbd8261aac1503e37ac520cf38b568b8bf614b01
Jair Aya <jair.aya@uniminuto.edu.co>
2022-11-02 20:33:22 -0500

    Nuevos componentes agregados

---
+++ components/Confirm.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function Confirm({ buttonTitle, title, content, buttonAction, disabled }) {
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleButtonAction = () => {
+        buttonAction();
+        handleClose();
+    }
+
+    return (
+        <>
+            <Button variant="contained" color='error' onClick={handleClickOpen} disabled={disabled}>
+                {buttonTitle}
+            </Button>
+
+            <Dialog
+                open={open}
+                onClose={handleClose}
+            >
+                <DialogTitle>
+                    {title}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        {content}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color='error'>Cancelar</Button>
+                    <Button onClick={handleButtonAction}>
+                        Aceptar
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    );
+}
---
+++ components/Dialog.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function FormDialog({ buttonTitle, title, label, buttonActionTitle, buttonAction }) {
+    const [open, setOpen] = useState(false);
+    const [error, setError] = useState(false);
+    const [input, setInput] = useState('');
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+        setInput('');
+        setError(false);
+    };
+
+    const handleButtonAction = () => {
+        if (!input) return setError(true);
+        buttonAction(input);
+        handleClose();
+    }
+
+    return (
+        <>
+            <Button variant="contained" onClick={handleClickOpen}>
+                {buttonTitle}
+            </Button>
+
+            <Dialog open={open} onClose={handleClose} maxWidth='sm' fullWidth={true} >
+                <DialogTitle>{title}</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label={label}
+                        type="number"
+                        fullWidth
+                        variant="standard"
+                        onChange={(e) => setInput(e.target.value)}
+                        error={error}
+                        helperText={error ? 'El campo es requerido' : ''}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button variant='contained' color='error' onClick={handleClose}>Cancelar</Button>
+                    <Button variant='contained' onClick={handleButtonAction}>{buttonActionTitle}</Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    );
+}
