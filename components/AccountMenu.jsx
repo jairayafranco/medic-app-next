76eb9096a8412e770562ed212c64fe58398b01bc
Jair Aya <jair.aya@uniminuto.edu.co>
2023-02-15 15:25:11 -0500

    fix logout bug

--- components/AccountMenu.jsx
+++ components/AccountMenu.jsx
@@ -108,7 +108,7 @@ export default function AccountMenu() {
                             return;
                         }
                         clearSessionStorageData();
-                        router.replace('/login');
+                        window.location.reload();
                     });
                 }}>
                     <ListItemIcon>
