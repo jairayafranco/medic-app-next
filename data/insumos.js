96938709bddc42b6299c1df39a2b47809f9630d6
Jair Aya <jair.aya@uniminuto.edu.co>
2023-02-20 17:10:50 -0500

    agregados datos

---
+++ data/insumos.js
@@ -0,0 +1,866 @@
+export const insumos = [
+    {
+        I1: "ACEITE MINERAL FRASCO X 500 ML."
+    },
+    {
+        I2: "ACEITE MINERAL GALON X 3800ML"
+    },
+    {
+        I3: "AGUA OXIGENADA FRASCO X 500 ML"
+    },
+    {
+        I4: "AGUJA HIPODERMICA DESECHABLE 16 G X 1 1/2"
+    },
+    {
+        I5: "AGUJA HIPODERMICA DESECHABLE 18 G X 1 1/2"
+    },
+    {
+        I6: "AGUJA HIPODERMICA DESECHABLE 19 G X 1 1/2"
+    },
+    {
+        I7: "AGUJA HIPODERMICA DESECHABLE 21 G X 1 1/2"
+    },
+    {
+        I8: "AGUJA HIPODERMICA DESECHABLE 22 G"
+    },
+    {
+        I9: "AGUJA HIPODERMICA DESECHABLE 23 G X 1"
+    },
+    {
+        I10: "AGUJA HIPODERMICA DESECHABLE 25 G X 5/8"
+    },
+    {
+        I11: "AGUJA HIPODERMICA N 20 G X 1 1/2"
+    },
+    {
+        I12: "AGUJA HIPODERMICA N 30. X 1/2"
+    },
+    {
+        I13: "AGUJA PARA PEN DE INSULINA 31 G X 8 MM"
+    },
+    {
+        I14: "AGUJA PARA PEN DE INSULINA 32 G X 4MM"
+    },
+    {
+        I15: "ALCOHOL ANTISEPTICO FRASCO X 750 ML"
+    },
+    {
+        I16: "ALCOHOL ANTISEPTICO GALON X 3700 ML"
+    },
+    {
+        I17: "ALCOHOL YODADO FRASCO X 500 ML"
+    },
+    {
+        I18: "ALGODON HOSPITALARIO ROLLO X 500 GR"
+    },
+    {
+        I19: "ALGODON TORUNDAS PAQUETE X 500 G"
+    },
+    {
+        I20: "APLICADOR DE MADERA CON ALGODON"
+    },
+    {
+        I21: "APOSITO ESTERIL ABSORBENTE 20X20 CMS"
+    },
+    {
+        I22: "APOSITO HIDROCOLOIDE 10 X20 CM"
+    },
+    {
+        I23: "APOSITO HIDROCOLOIDE 10X10 CMS"
+    },
+    {
+        I24: "APOSITO HIDROCOLOIDE 7.5 X 15 CM"
+    },
+    {
+        I25: "APOSITO HIDROCOLOIDE 7.5 X 7.5 CM"
+    },
+    {
+        I26: "APOSITO OCULAR AUTOADHESIVO ADULTO"
+    },
+    {
+        I27: "BAJALENGUAS DE MADERA"
+    },
+    {
+        I28: "BANDA MICROPERFORADA ADHESIVA ESTANDAR (CURAS)"
+    },
+    {
+        I29: "BANDA MICROPERFORADA ADHESIVA REDONDA (CURAS)"
+    },
+    {
+        I30: "BARRERA PARA BOLSA DE COLOSTOMIA No 57"
+    },
+    {
+        I31: "BARRERA PARA BOLSA DE COLOSTOMIA No 70"
+    },
+    {
+        I32: "BATA LARGA IMPERMEABLE NO ESTERIL"
+    },
+    {
+        I33: "BATA PARA PACIENTE"
+    },
+    {
+        I34: "BOLSA DE COLOSTOMIA CON BARRERA N° 45"
+    },
+    {
+        I35: "BOLSA DE COLOSTOMIA CON BARRERA N° 57"
+    },
+    {
+        I36: "BOLSA DE COLOSTOMIA CON BARRERA N° 70"
+    },
+    {
+        I37: "BOLSA PARA ALIMENTACION ENTERAL 1.5 L"
+    },
+    {
+        I38: "BOLSA PARA COLOSTOMIA AJUSTABLE No 57"
+    },
+    {
+        I39: "BOLSA PARA COLOSTOMIA AJUSTABLE No 70"
+    },
+    {
+        I40: "BOLSA PARA ESTERILIZAR GRANDE"
+    },
+    {
+        I41: "BOLSA PARA ESTERILIZAR MEDIANA"
+    },
+    {
+        I42: "BOLSA PARA ESTERILIZAR PEQUEÑA"
+    },
+    {
+        I43: "BOLSA RECOLECTORA DE ORINA CAPACIDAD 2000 CC"
+    },
+    {
+        I44: "CAMPO DESECHABLE NO ESTERIL 0.45   X   0.45 M."
+    },
+    {
+        I45: "CANULA GUEDELL Y/O MAYO. NO.1\n"
+    },
+    {
+        I46: "CANULA GUEDELL Y/O MAYO. NO.4\n"
+    },
+    {
+        I47: "CANULA GUEDELL Y/O MAYO. NO.5\n"
+    },
+    {
+        I48: "CANULA NASAL OXIGENO PEDIATRICO"
+    },
+    {
+        I49: "CANULA NASAL PARA ADMINISTRACION DE OXIGENO ADULTO "
+    },
+    {
+        I50: "CATETER INTRAVENOSO NO 16"
+    },
+    {
+        I51: "CATETER INTRAVENOSO NO 18"
+    },
+    {
+        I52: "CATETER INTRAVENOSO NO 20"
+    },
+    {
+        I53: "CATETER INTRAVENOSO NO 22"
+    },
+    {
+        I54: "CATETER INTRAVENOSO NO 24"
+    },
+    {
+        I55: "CATGUT CROMADO  3-0 CON AGUJA CURVA"
+    },
+    {
+        I56: "CATGUT CROMADO 0 CON AGUJA"
+    },
+    {
+        I57: "CATGUT CROMADO 2-0 CON AGUJA CURVA"
+    },
+    {
+        I58: "CATGUT CROMADO 4-0 CON AGUJA CURVA"
+    },
+    {
+        I59: "CATGUT CROMADO 5-0 CON AGUJA CURVA"
+    },
+    {
+        I60: "CATGUT SIMPLE 2-0 CON AGUJA CURVA"
+    },
+    {
+        I61: "CATGUT SIMPLE 3-0 CON AGUJA CURVA REDONDA"
+    },
+    {
+        I62: "CATGUT SIMPLE 4-0 CON AGUJA"
+    },
+    {
+        I63: "CEPILLO PARA LAVADO DE INSTRUMENTAL"
+    },
+    {
+        I64: "CLIP PLASTICO PARA COLOSTOMIA"
+    },
+    {
+        I65: "COMPRESAS ESTERILES GASA 45X45 CM"
+    },
+    {
+        I66: "CONOS  DESECHABLES PARA OTOSCOPIO 4.25 mm"
+    },
+    {
+        I67: "CUELLO ORTOPEDICO INMOVILIZACION CERVICAL ADULTO"
+    },
+    {
+        I68: "DETERGENTE MULTIENZIMATICO CONCENTRADO GALON"
+    },
+    {
+        I69: "DREN DE PEN-ROSSE DE 1/2"
+    },
+    {
+        I70: "ELECTRODOS PARA MONITOREO DE SIGNOS VITALES ADULTO"
+    },
+    {
+        I71: "EQUIPO BURETROL 150 ML"
+    },
+    {
+        I72: "EQUIPO DE MACROGOTEO SIN AGUJA"
+    },
+    {
+        I73: "EQUIPO MACROGOTEO CON AGUJA"
+    },
+    {
+        I74: "EQUIPO MICROGOTEO SIN AGUJA"
+    },
+    {
+        I75: "ESPARADRAPO DE TELA ROLLO DE 1\" "
+    },
+    {
+        I76: "ESPARADRAPO DE TELA ROLLO DE 2\" "
+    },
+    {
+        I77: "ESPARADRAPO DE TELA ROLLO DE 3\" "
+    },
+    {
+        I78: "ESPARADRAPO MICROPOROSO ROLLO 1\""
+    },
+    {
+        I79: "ESPARADRAPO MICROPOROSO ROLLO 2\""
+    },
+    {
+        I80: "ESPARADRAPO MICROPOROSO ROLLO 3\""
+    },
+    {
+        I81: "ESPECULO DESECHABLE PARA OTOSCOPIO 2.75 mm"
+    },
+    {
+        I82: "FIJADOR CITOLOGICO SPRAY X 150 ML"
+    },
+    {
+        I83: "GASA PRECORTADA ESTERIL  7.5 X 7.5 CM SOBRE"
+    },
+    {
+        I84: "GASA ROLLO DE 1 X 90 YARDAS"
+    },
+    {
+        I85: "GEL ANTIBACTERIAL PARA LIMPIEZA DE MANOS FRASCO X 1 LITRO"
+    },
+    {
+        I86: "GEL ANTIBACTERIAL RECIPIENTE PLASTICO GALON"
+    },
+    {
+        I87: "GEL HIDROACTIVO TUBO X 15 GR"
+    },
+    {
+        I88: "GLICERINA PURA 500 CC"
+    },
+    {
+        I89: "GLUCOMETRO PORTATIL"
+    },
+    {
+        I90: "GLUTARALDEHIDO 2% GALON X 3700 ML"
+    },
+    {
+        I91: "GORROS DESECHABLES "
+    },
+    {
+        I92: "GUANTE DE CAUCHO 6 1/2 PAR"
+    },
+    {
+        I93: "GUANTE DE CAUCHO 7 1/2 PAR"
+    },
+    {
+        I94: "GUANTES ESTERILES NO. 6.5 PAR"
+    },
+    {
+        I95: "GUANTES ESTERILES NO. 7.0 PAR"
+    },
+    {
+        I96: "GUANTES ESTERILES NO. 7.5 PAR"
+    },
+    {
+        I97: "GUANTES ESTERILES NO. 8.0 PAR"
+    },
+    {
+        I98: "GUANTES PARA EXAMEN TALLA L"
+    },
+    {
+        I99: "GUANTES PARA EXAMEN TALLA M"
+    },
+    {
+        I100: "GUANTES PARA EXAMEN TALLA S"
+    },
+    {
+        I101: "GUANTES PARA EXAMEN TALLA XS"
+    },
+    {
+        I102: "GUARDIAN DE 1 LITRO"
+    },
+    {
+        I103: "GUARDIAN DE 3 LITROS"
+    },
+    {
+        I104: "GUIA DE INTUBACION ADULTO"
+    },
+    {
+        I105: "HIPOCLORITO DE SODIO AL 5% GARRAFA X 3.800 CC "
+    },
+    {
+        I106: "HIPOCLORITO DE SODIO TABLETA FRASCO X 100"
+    },
+    {
+        I107: "HOJAS DE BISTURI NO 10"
+    },
+    {
+        I108: "HOJAS DE BISTURI NO 11"
+    },
+    {
+        I109: "HOJAS DE BISTURI NO 15"
+    },
+    {
+        I110: "HOJAS DE BISTURI NO 20"
+    },
+    {
+        I111: "HOJAS DE BISTURI NO 22"
+    },
+    {
+        I112: "HUMIDIFICADOR DE OXIGENO "
+    },
+    {
+        I113: "INMOVILIZADOR DE EXTREMIDADES"
+    },
+    {
+        I114: "JABON ANTISEPTICO PARA LAVADO DE MANOS GALON X 3800 ML"
+    },
+    {
+        I115: "JABON QUIRURGICO A BASE DE YODO FRASCO X 1 LITRO"
+    },
+    {
+        I116: "JALEA CONDUCTORA PARA ULTRASONIDO FRASCO X 250 ML"
+    },
+    {
+        I117: "JERINGA 60 ML 3P PUNTA CATETER"
+    },
+    {
+        I118: "JERINGA DE 10 ML 3P CON AGUJA DE 21X 1 1/2"
+    },
+    {
+        I119: "JERINGA DE 20 ML 2P CON AGUJA 21 X 1 1/2"
+    },
+    {
+        I120: "JERINGA DE 20 ML 3P SIN AGUJA"
+    },
+    {
+        I121: "JERINGA DE 3 ML 3P CON AGUJA DE 21X 1 1/2"
+    },
+    {
+        I122: "JERINGA DE 3ML 3P CON AGUJA DE 23 X 1"
+    },
+    {
+        I123: "JERINGA DE 5 ML 3P CON AGUJA DE 21X1 1/2"
+    },
+    {
+        I124: "JERINGA DE 50ML 3P SIN AGUJA"
+    },
+    {
+        I125: "JERINGA DE 5ML 2P CON AGUJA DE 21 X 1 1/2"
+    },
+    {
+        I126: "JERINGA DE INSULINA 1 ML 3P CON AGUJA 27 X 1/2"
+    },
+    {
+        I127: "JERINGA DE INSULINA 1 ML 3P CON AGUJA 30 X 1/2"
+    },
+    {
+        I128: "JERINGA DESECHABLE X 20 ML /A"
+    },
+    {
+        I129: "KIT DE CITOLOGIA (ESPECULO+ESPATULA+CEPILLO+LAMINA)"
+    },
+    {
+        I130: "LANCETAS PARA GLUCOMETRIA"
+    },
+    {
+        I131: "MANGUERA PLASTICA PARA SUCCION 1/4 X 2 M"
+    },
+    {
+        I132: "MASCARA DE OXIGENO ADULTO 2.1 MT"
+    },
+    {
+        I133: "MASCARA LARINGEA ADULTO"
+    },
+    {
+        I134: "MASCARA OXIGENO ADULTO CON RESERVORIO"
+    },
+    {
+        I135: "MICRONEBULIZADOR ADULTO"
+    },
+    {
+        I136: "MICRONEBULIZADOR PEDIATRICO"
+    },
+    {
+        I137: "PAPEL ELECTRO  5 X 30 CM ROLLO"
+    },
+    {
+        I138: "PAPEL ELECTRO 112 MM X 27 M ROLLO"
+    },
+    {
+        I139: "PAPEL ELECTROCARDIOGRAMA EN Z 400 HOJAS DE 90 X 90 MM"
+    },
+    {
+        I140: "PAPEL GRADO MEDICO ROLLO 12X100"
+    },
+    {
+        I141: "PAPEL GRADO MEDICO ROLLO 15X100"
+    },
+    {
+        I142: "PAPEL GRADO MEDICO ROLLO 5X100"
+    },
+    {
+        I143: "PRESERVATIVO DE LATEX"
+    },
+    {
+        I144: "RESUCITADOR DESECHABLE ADULTO"
+    },
+    {
+        I145: "SABANAS DESECHABLES AJUSTABLE PARA CAMILLA"
+    },
+    {
+        I146: "SEDA NEGRA TRENZADA 1-0 CON AGUJA CURVA CORTANTE"
+    },
+    {
+        I147: "SEDA NEGRA TRENZADA 2-0 CON AGUJA CURVA CORTANTE"
+    },
+    {
+        I148: "SEDA NEGRA TRENZADA 3-0 CON AGUJA CURVA CORTANTE"
+    },
+    {
+        I149: "SEDA NEGRA TRENZADA 4-0 CON AGUJA CURVA CORTANTE"
+    },
+    {
+        I150: "SEDA NEGRA TRENZADA 5-0 CON AGUJA CURVA CORTANTE"
+    },
+    {
+        I151: "SOLUCION DESINFECTANTE A BASE DE YODO FRASCO 120 ML"
+    },
+    {
+        I152: "SOLUCION DESINFECTANTE A BASE DE YODO GALON X 3500 ML"
+    },
+    {
+        I153: "SONDA FOLLEY NO 12"
+    },
+    {
+        I154: "SONDA FOLLEY NO 14"
+    },
+    {
+        I155: "SONDA FOLLEY NO 16"
+    },
+    {
+        I156: "SONDA FOLLEY NO 18"
+    },
+    {
+        I157: "SONDA FOLLEY NO 20"
+    },
+    {
+        I158: "SONDA FOLLEY NO 22"
+    },
+    {
+        I159: "SONDA FOLLEY NO 24"
+    },
+    {
+        I160: "SONDA FOLLEY NO 8"
+    },
+    {
+        I161: "SONDA NASOGASTRICA NO 12"
+    },
+    {
+        I162: "SONDA NASOGASTRICA NO 14"
+    },
+    {
+        I163: "SONDA NASOGASTRICA NO 16"
+    },
+    {
+        I164: "SONDA NASOGASTRICA NO 18"
+    },
+    {
+        I165: "SONDA NASOGASTRICA NO 20"
+    },
+    {
+        I166: "SONDA NELATON NO 10"
+    },
+    {
+        I167: "SONDA NELATON NO 12"
+    },
+    {
+        I168: "SONDA NELATON NO 14"
+    },
+    {
+        I169: "SONDA NELATON NO 16"
+    },
+    {
+        I170: "SONDA NELATON NO 18"
+    },
+    {
+        I171: "SONDA NELATON NO 20"
+    },
+    {
+        I172: "SONDA NELATON NO 8"
+    },
+    {
+        I173: "TAPABOCAS  N 95"
+    },
+    {
+        I174: "TAPABOCAS CON BARRERA ANTIFLUIDOS"
+    },
+    {
+        I175: "TAPON HEPARINIZADO"
+    },
+    {
+        I176: "TERMOMETRO ORAL"
+    },
+    {
+        I177: "TINTURA DE BENJUI FRASCO X 500 ML"
+    },
+    {
+        I178: "TIRAS DE GLUCOMETRIA"
+    },
+    {
+        I179: "TIRAS REACTIVAS DE UROANALISIS"
+    },
+    {
+        I180: "TOALLAS DE PAPEL PARA MANOS"
+    },
+    {
+        I181: "TUBO ENDOTRAQUEAL NO 5"
+    },
+    {
+        I182: "TUBO ENDOTRAQUEAL NO 6"
+    },
+    {
+        I183: "TUBO ENDOTRAQUEAL NO 7"
+    },
+    {
+        I184: "TUBO ENDOTRAQUEAL NO 7.5"
+    },
+    {
+        I185: "TUBO ENDOTRAQUEAL NO 8"
+    },
+    {
+        I186: "TUBO ENDOTRAQUEAL NO 8.5"
+    },
+    {
+        I187: "VASELINA FRASCO X 500 GR"
+    },
+    {
+        I188: "VENDA ALGODON LAMINADO 3X5 YARDAS"
+    },
+    {
+        I189: "VENDA ALGODON LAMINADO 4X5 YARDAS"
+    },
+    {
+        I190: "VENDA ALGODON LAMINADO 5X5 YARDAS"
+    },
+    {
+        I191: "VENDA ALGODON LAMINADO 6X5 YARDAS"
+    },
+    {
+        I192: "VENDA DE GASA 2X5 YARDAS"
+    },
+    {
+        I193: "VENDA DE GASA 6X5 YARDAS"
+    },
+    {
+        I194: "VENDA DE TELA 4X5 YARDAS"
+    },
+    {
+        I195: "VENDA DE TELA 6X5 YARDAS"
+    },
+    {
+        I196: "VENDA ELASTICA 3X5 YARDAS"
+    },
+    {
+        I197: "VENDA ELASTICA 4X5 YARDAS"
+    },
+    {
+        I198: "VENDA ELASTICA 5X5 YARDAS"
+    },
+    {
+        I199: "VENDA ELASTICA 6X5 YARDAS"
+    },
+    {
+        I200: "VENDAJE DE YESO 3X5 YARDAS"
+    },
+    {
+        I201: "VENDAJE DE YESO 4X5 YARDAS"
+    },
+    {
+        I202: "VENDAJE DE YESO 5X5 YARDAS"
+    },
+    {
+        I203: "VENDAJE DE YESO 6X5 YARDAS"
+    },
+    {
+        I204: "YODOPOVIDONA ESPUMA FRASCO X 120 ML"
+    },
+    {
+        I205: "GUARDIAN DE 1.5 LITROS"
+    },
+    {
+        I206: "BOLSA PARA COLOSTOMIA AJUSTABLE No 45"
+    },
+    {
+        I207: "BARRERA PARA BOLSA DE COLOSTOMIA No 45"
+    },
+    {
+        I208: "AGUJA HIPODERMICA Nº. 24 G X 1\" ."
+    },
+    {
+        I209: "BOLSA COLOSTOMIA ( CONVATEC REF 402566 ) 32 MM ."
+    },
+    {
+        I210: "BOLSA VACIA PARA ENEMA ( TRAVAD ) . ."
+    },
+    {
+        I211: "BOLSA PARA RECOLECCION DE ORINA PEDIATRICA . ."
+    },
+    {
+        I212: "CINTA CONTROL A VAPOR ( TESTIGO ESTERITECH ) . ."
+    },
+    {
+        I213: "EQUIPO PARA TRANSFUSION ( ADMINISTRACION ) DE SANGRE SIN AGUJA ."
+    },
+    {
+        I214: "FORMOL . LIQUIDO"
+    },
+    {
+        I215: "GUANTE QUIRURGICO ESTERIL PAR Nº. 8.5 ."
+    },
+    {
+        I216: "MASCARA PARA OXIGENO PEDIATRICA . ."
+    },
+    {
+        I217: "POLAINA DESECHABLE ( UNIDAD ) . ."
+    },
+    {
+        I218: "POLIPROPILENO AZUL MONOFILAMENTO ( PROLENE REF: 8423T ) 2-0 /A 75 CM CT-1 ."
+    },
+    {
+        I219: "RECOLECTOR COPROLOGICO . ."
+    },
+    {
+        I220: "RECOLECTOR DE ORINA . ."
+    },
+    {
+        I221: "SONDA FOLEY 3 VIAS Nº. 22 ."
+    },
+    {
+        I222: "SONDA FOLEY 3 VIAS Nº. 24 ."
+    },
+    {
+        I223: "SONDA FOLEY 2 VIAS Nº. 10 ."
+    },
+    {
+        I224: "SONDA NASOGASTRICA LEVIN Nº. 06 ."
+    },
+    {
+        I225: "SONDA NASOGASTRICA LEVIN Nº. 08"
+    },
+    {
+        I226: "SONDA NELATON Nº. 05"
+    },
+    {
+        I227: "SONDA NELATON Nº. 06 ."
+    },
+    {
+        I228: "SONDA SUCCION Nº. 10 ."
+    },
+    {
+        I229: "SONDA SUCCION Nº. 12 ."
+    },
+    {
+        I230: "SONDA SUCCION Nº. 14 ."
+    },
+    {
+        I231: "SONDA SUCCION Nº. 16 ."
+    },
+    {
+        I232: "SONDA SUCCION Nº. 08 ."
+    },
+    {
+        I233: "APOSITO ESTERIL DUODERM CGF ( DUODERM ) 10 CM X 10 CM DISPOSITIVO TRANSDERMICO Y OSMOTICO"
+    },
+    {
+        I234: "RECOLECTOR DE ELEMENTOS CORTOPUNZANTES ( GUARDIAN ) ( PEQUEÑO ) 0.3 LT ."
+    },
+    {
+        I235: "VENDA DE TELA FIJA ( ESPECIALIZADA III ) 3\" X 5 YDS ."
+    },
+    {
+        I236: "SEDA NEGRA TRENZADA 0 S/A 75 CM ."
+    },
+    {
+        I237: "POLIPROPILENO AZUL MONOFILAMENTO ( PROLENE REF: 8623H ) 2-0 /A 75 CM KS ."
+    },
+    {
+        I238: "ENZIMAS PROTEOLITICAS DETERGENTE ENZIMATICO ( CIDEZYME ) . LIQUIDO"
+    },
+    {
+        I239: "POLIPROPILENO AZUL MONOFILAMENTO ( PROLENE ) 2-0 /A 75 CM CT-2 ."
+    },
+    {
+        I240: "POLIPROPILENO AZUL MONOFILAMENTO ( PROLENE ) 4-0 D/A 75 CM RB-1 / RB-1"
+    },
+    {
+        I241: "POLIPROPILENO AZUL MONOFILAMENTO ( PROLENE REF: 9556T ) 5-0 D/A 75 CM RB-1 / RB-1 ."
+    },
+    {
+        I242: "POLIPROPILENO AZUL MONOFILAMENTO 5-0 /A 45 CM"
+    },
+    {
+        I243: "POLIPROPILENO AZUL MONOFILAMENTO ( PROLENE REF: 8697T ) 6-0 /A 45 CM P-1 ."
+    },
+    {
+        I244: "BATA QUIRURGICA ESTERIL ( ANTIFLUIDO ) . ."
+    },
+    {
+        I245: "AGUJA HIPODERMICA Nº. 26 G X 1/2\" ."
+    },
+    {
+        I246: "AGUJA HIPODERMICA Nº. 27 G X 1/2\" ."
+    },
+    {
+        I247: "JERINGA 1 ML 3P .A. Nº. 25 G X 5/8\" ."
+    },
+    {
+        I248: "PAÑAL PARA ADULTO ( TENA SLIP TALLA M ) . ."
+    },
+    {
+        I249: "EQUIPO PARA BOMBA DE INFUSION ( TERUMO ) . ."
+    },
+    {
+        I250: "MICROPORE COLOR BLANCO 1/2\" X 10 YD ( 1.2 CM X 9.1 M )"
+    },
+    {
+        I251: "MICROPORE COLOR PIEL 1/2\" X 10 YD ( 1.25 CM X 9 M ) ."
+    },
+    {
+        I252: "CONECTOR DE OXIGENO ( NIPLES ) ( 1/4*3/8 SIN LUER ) . ."
+    },
+    {
+        I253: "CATETER SONDA TUBO PARA DRENAJE DE TORAX Nº. 20"
+    },
+    {
+        I254: "POLIPROPILENO AZUL MONOFILAMENTO ( PROLENE REF: 8160T ) 6-0 /A 45 CM SC-16 ."
+    },
+    {
+        I255: "POLIPROPILENO AZUL MONOFILAMENTO ( PROLENE REF: M-8702T ) 7-0 D/A 60 CM BV-1 / BV-1 ."
+    },
+    {
+        I256: "SEDA NEGRA TRENZADA 6-0 /A 45 CM DS-12 ."
+    },
+    {
+        I257: "TUBO ENDOTRAQUEAL CON BALON Nº. 9.0 ."
+    },
+    {
+        I258: "NYLON NEGRO MONOFILAMENTO ( ETHILON MONONYLON REF: 7717 OFTALMOLOGIA ) 9-0 D/A 30 CM TG-140-8 / TG-140-8 PLUS ."
+    },
+    {
+        I259: "NYLON NEGRO MONOFILAMENTO ( ETHILON MONONYLON REF: 164T ) 2-0 /A 45 CM SC-26 ."
+    },
+    {
+        I260: "NYLON NEGRO MONOFILAMENTO 5-0 /A 45 CM SC-20 ."
+    },
+    {
+        I261: "AGUJA HIPODERMICA Nº. 18 G X 1 1/4\""
+    },
+    {
+        I262: "BOLSA PARA NUTRICION PARENTERAL ( EVA ) . ."
+    },
+    {
+        I263: "APOSITO ESTERIL TRANSPARENTE TEGADERM 10 CM X 25 CM DISPOSITIVO TRANSDERMICO Y OSMOTICO"
+    },
+    {
+        I264: "SONDA SUCCION CERRADA ( TRACH CARE REF: 22106-4 ) Nº. 14 FR ."
+    },
+    {
+        I265: "APOSITO ESTERIL TRANSPARENTE TEGADERM ( FIXAPRO ) 10 CM X 12 CM DISPOSITIVO TRANSDERMICO Y OSMOTICO"
+    },
+    {
+        I266: "CLORHEXIDINA GLUCONATO JABON QUIRURGICO ( QUIRUCIDAL ) 3-4 % JABON"
+    },
+    {
+        I267: "AGUJA HIPODERMICA Nº. 25 G X 1 1/2\""
+    },
+    {
+        I268: "CONECTOR EN T PARA TERAPIA . ."
+    },
+    {
+        I269: "JERINGA 1 ML 3P .A. Nº. 29 G X 1/2\" ."
+    },
+    {
+        I270: "APOSITO ESTERIL DUODERM EXTRA DELGADO ( THIN ) 15 CM X 15 CM DISPOSITIVO TRANSDERMICO Y OSMOTICO"
+    },
+    {
+        I271: "NYLON NEGRO MONOFILAMENTO ( ETHILON REF: W-2813 ) 9-0 /A 13 CM BV-130-4 ."
+    },
+    {
+        I272: "SONDA SUCCION CERRADA Nº. 12 FR ."
+    },
+    {
+        I273: "AGUJAS PARA RECOLECCION DE MUESTRAS DE SANGRE ( VACUTAINER ) ."
+    },
+    {
+        I274: "TUBO AL VACIO TAPA VACUTAINER... ( VERDE CON NAHEP ) 4 ML"
+    },
+    {
+        I275: "TIRAS ABRASIVAS DE POLIESTER . ."
+    },
+    {
+        I276: "APOSITO SUPER ABSORBENTE DE SILICONA ( CUTIMED SILTEC ) . ."
+    },
+    {
+        I277: "APOSITO QUIRURGICO ( GASA ADHESIVA ) ( FIXODEMA ) 5 CM X 10 M DISPOSITIVO TRANSDERMICO Y OSMOTICO"
+    },
+    {
+        I278: "INDICADORES QUIMICOS Y BIOLOGICOS PARA MONITORIZAR PROCESOS DE ESTERILIZACION . ."
+    },
+    {
+        I279: "CINTA TESTIGO CT20 ( CINTAPE ) . ."
+    },
+    {
+        I280: "APOSITO DE HIDROFIBRA AQUACEL ( AQUACEL AG EXTRA ) 15 X 15 CM DISPOSITIVO TRANSDERMICO Y OSMOTICO"
+    },
+    {
+        I281: "POLIPROPILENO AZUL MONOFILAMENTO ( SURGIPRO SP-622 ) 3-0 /A 75 CM SC-2 ."
+    },
+    {
+        I282: "APOSITO DUODERM CON GEL CGF 15CM X 20 CM DISPOSITIVO TRANSDERMICO Y OSMOTICO"
+    },
+    {
+        I283: "POLIPROPILENO AZUL MONIFILAMENTO ( DEMELENE ) 1/0 .A 90 CM DCT-1 ."
+    },
+    {
+        I284: "JERINGA 1 ML 3P .A N° 31 G X 1/4\" ."
+    },
+    {
+        I285: "PAÑALES ADULTOS"
+    },
+    {
+        I286: "OXIGENO DOMICILIARIO"
+    },
+    {
+        I287: "BALA DE OXIGENO PORTATIL"
+    },
+    {
+        I288: "PAÑITOS HUMEDOS PAQUETE X 100"
+    }
+]
