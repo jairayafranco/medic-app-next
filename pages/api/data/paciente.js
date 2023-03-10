fb585e7d61eddee69f686cbc95b5f3671fad8eca
Jair Aya <jair.aya@uniminuto.edu.co>
2023-03-10 15:05:47 -0500

    agregada auditoria y mejoradas las peticiones

--- pages/api/data/paciente.js
+++ pages/api/data/paciente.js
@@ -1,7 +1,8 @@
 import conn from '../../../config/db_config';
+import jwt from 'jsonwebtoken';
 import { lodash as _ } from '../../../lib/lodash';
 
-export default function pacienteHandler(req, res) {
+export default async function pacienteHandler(req, res) {
     const db = conn.collection('pacientes');
     const dbSV = conn.collection('signosVitalesHistory');
     const handleException = ({ code, message, status, type }) => res.status(code).json({ status, message, type })
@@ -10,7 +11,7 @@ export default function pacienteHandler(req, res) {
         const { id } = req.query;
         if (!id) return handleException({ code: 400, message: 'Identificacion requerida', status: false, type: 'warning' });
 
-        db.findOne({ "datosBasicos.idUsuario": Number(id) }, { projection: { _id: 0 } })
+        db.findOne({ "datosBasicos.idUsuario": Number(id), estado: "ACTIVO" }, { projection: { _id: 0, estado: 0 } })
             .then((paciente) => {
                 if (!paciente) return handleException({ code: 404, message: 'Paciente no encontrado', status: false, type: 'warning' });
                 dbSV.find({ idUsuario: Number(id) }, { projection: { _id: 0, idUsuario: 0 } }).toArray()
@@ -32,8 +33,9 @@ export default function pacienteHandler(req, res) {
 
         db.findOne({ "datosBasicos.idUsuario": data.idUsuario }).then((paciente) => {
             if (paciente) return handleException({ code: 400, status: false, message: 'El paciente ya existe', type: 'warning' });
-            db.insertOne({ datosBasicos: data })
+            db.insertOne({ datosBasicos: data, estado: "ACTIVO" })
                 .then(() => {
+                    saveAuditData({ action: 'CREAR PACIENTE', req, data: data.idUsuario });
                     return res.status(200).json({ status: true, message: 'Paciente creado correctamente', type: 'success' });
                 }).catch(() => {
                     return handleException({ code: 500, status: false, message: 'Error al crear el paciente', type: 'error' });
@@ -43,34 +45,34 @@ export default function pacienteHandler(req, res) {
         });
     }
 
-    if (req.method === "PUT") {
+    if (["PUT", "PATCH"].includes(req.method)) {
+        const method = req.method;
         const data = req.body;
         const { id, opt } = req.query;
         if (!data || !id || !opt) return handleException({ code: 400, status: false, message: 'Datos requeridos', type: 'warning' });
 
-        db.findOneAndUpdate({ "datosBasicos.idUsuario": Number(id) }, { $set: { [opt]: data } })
-            .then((paciente) => {
-                if (!paciente.value) return handleException({ code: 404, status: false, message: 'Paciente no encontrado', type: 'warning' });
-                return res.status(200).json({ status: true, message: 'Paciente actualizado correctamente', type: 'success' });
-            }).catch(() => {
-                return handleException({ code: 500, status: false, message: 'Error al actualizar el paciente', type: 'error' });
-            });
-    }
-
-    if (req.method === "PATCH") {
-        const data = req.body;
-        const { id, opt } = req.query;
-        if (!data || !id || !opt) return handleException({ code: 400, status: false, message: 'Datos requeridos', type: 'warning' });
+        if (data.idUsuario) {
+            const paciente = await db.findOne({ "datosBasicos.idUsuario": Number(data.idUsuario) });
+            if (paciente) return handleException({ code: 400, status: false, message: 'La identificacion ya existe', type: 'warning' });
+        }
 
         const update = { $set: {} };
-        for (const key in data) {
-            update.$set[opt + "." + key] = data[key];
+        if (method === "PUT") {
+            update.$set = { [opt]: data };
+        } else {
+            for (const key in data) {
+                update.$set[opt + "." + key] = data[key];
+            }
         }
 
         db.findOneAndUpdate({ "datosBasicos.idUsuario": Number(id) }, update)
-            .then((paciente) => {
-                if (!paciente.value) return handleException({ code: 404, status: false, message: 'Paciente no encontrado', type: 'warning' });
-                return res.status(200).json({ status: true, message: 'Paciente actualizado correctamente' });
+            .then(({ value: paciente }) => {
+                if (!paciente) return handleException({ code: 404, status: false, message: 'Paciente no encontrado', type: 'warning' });
+                if (data.idUsuario && (paciente.datosBasicos.idUsuario !== data.idUsuario)) {
+                    dbSV.updateMany({ idUsuario: Number(id) }, { $set: { idUsuario: Number(data.idUsuario) } });
+                }
+                saveAuditData({ action: 'ACTUALIZAR PACIENTE', req, data: id });
+                return res.status(200).json({ status: true, message: 'Paciente actualizado correctamente', type: 'success' });
             }).catch(() => {
                 return handleException({ code: 500, status: false, message: 'Error al actualizar el paciente', type: 'error' });
             });
@@ -80,12 +82,32 @@ export default function pacienteHandler(req, res) {
         const { id } = req.query;
         if (!id) return handleException({ code: 400, status: false, message: 'Identificacion requerida', type: 'warning' })
 
-        db.findOneAndDelete({ "datosBasicos.idUsuario": Number(id) })
+        db.findOneAndUpdate({ "datosBasicos.idUsuario": Number(id) }, { $set: { estado: "INACTIVO" } })
             .then((paciente) => {
                 if (!paciente.value) return handleException({ code: 404, status: false, message: 'Paciente no encontrado', type: 'warning' });
+                saveAuditData({ action: 'ELIMINAR PACIENTE', req, data: id });
                 return res.status(200).json({ status: true, message: 'Paciente eliminado correctamente', type: 'success' });
             }).catch(() => {
                 return handleException({ code: 500, status: false, message: 'Error al eliminar el paciente', type: 'error' });
             });
+    }
+
+    function saveAuditData({ action, req, data }) {
+        try {
+            const token = req.headers.cookie.substring(6);
+            const { username, cc, admin } = jwt.verify(token, process.env.SECRET);
+            const newAuditData = {
+                admin,
+                username,
+                cc: cc || '',
+                action,
+                date: new Date().toLocaleString(),
+                data
+            }
+            const db = conn.collection('auditoria');
+            db.insertOne(newAuditData);
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
