0d1f589d88bd976765b24d10ef016d09b03b6006
Jair Aya <jair.aya@uniminuto.edu.co>
2023-04-07 21:26:54 -0500

    minor fix

--- store/useThemeStore.js
+++ store/useThemeStore.js
@@ -2,12 +2,14 @@ import { create } from 'zustand';
 import { createTheme } from '@mui/material/styles';
 import { esES } from '@mui/x-data-grid';
 
+const isWindow = typeof window !== "undefined";
+
 const useThemeStore = create((set, get) => ({
-    theme: typeof window !== "undefined" && localStorage.getItem('MUIThemeMode') || 'light',
+    theme: isWindow && localStorage.getItem('MUIThemeMode') || 'light',
     themeMode: () => createTheme({ palette: { mode: get().theme } }, esES),
     setTheme: (theme) => {
         set({ theme });
-        typeof window !== "undefined" && localStorage.setItem('MUIThemeMode', theme);
+        isWindow && localStorage.setItem('MUIThemeMode', theme);
     }
 }));
 
