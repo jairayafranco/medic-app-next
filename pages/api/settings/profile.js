9b14a3efb9458c0cf199d488c604afd11ee94bcd
Jair Aya <jair.aya@uniminuto.edu.co>
2023-04-09 22:09:02 -0500

    fix logout

--- pages/api/settings/profile.js
+++ pages/api/settings/profile.js
@@ -2,7 +2,7 @@ import { verify } from "jsonwebtoken";
 
 export default function profileHandler(req, res) {
     const { token } = req.cookies;
-    if (!token) return res.status(401).json({ message: 'No token' });
+    if (!token) return;
 
     verify(token, process.env.SECRET, (err, decoded) => {
         if (err) return res.status(500).json({ message: 'Error al verificar token' });
