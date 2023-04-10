04375369a8cccd2e44967b01fba336db71e5d547
Jair Aya <jair.aya@uniminuto.edu.co>
2023-04-09 21:33:06 -0500

    ajustes a signosVitales

--- pages/panel/signos-vitales.jsx
+++ pages/panel/signos-vitales.jsx
@@ -4,12 +4,8 @@ import ButtonGroup from '@mui/material/ButtonGroup';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import InputAdornment from '@mui/material/InputAdornment';
-import { useEffect, useState } from 'react'
 import { signosVitalesSchema } from '../../schemas/schemas';
-import { AppContext } from '../../context/AppContext';
 import {
-    saveSessionStorageData,
-    getSessionStorageData,
     moduleCompleted,
     signosVitalesColorSchema,
     handleSVInputValues,
@@ -19,33 +15,25 @@ import { saveSignosVitalesHistory } from '../../services/axiosApi';
 import FullScreenModal from '../../components/FullScreenModal';
 import DataTable from '../../components/DataTable';
 import { signosVitalesFields } from '../../data/inputs';
+import usePacienteStore from '../../store/usePacienteStore';
+import { useFormik } from 'formik';
+import useUpdate from '../../hooks/useUpdate';
 
 export default function SignosVitales() {
-    const { updateModule } = AppContext();
-    const [history, setHistory] = useState([]);
+    const { paciente, setPacienteModule } = usePacienteStore();
+    const updatePaciente = useUpdate();
 
-    useEffect(() => {
-        const data = getSessionStorageData("signosVitales");
-        const history = getSessionStorageData("signosVitalesHistory") || [];
-        if (data) {
-            formik.setValues(data);
-            setHistory(history);
+    const formik = useFormik({
+        initialValues: paciente.signosVitales || formatInitialValues(signosVitalesFields),
+        validationSchema: signosVitalesSchema,
+        onSubmit: (data) => {
+            updatePaciente({ module: "signosVitales", data });
+            saveSignosVitalesHistory(paciente.datosBasicos.idUsuario, data).then(({ newHistoryRegister }) => {
+                const orderHistory = [...paciente.signosVitalesHistory];
+                orderHistory.unshift(newHistoryRegister);
+                setPacienteModule("signosVitalesHistory", orderHistory);
+            }).catch(err => console.log(err));
         }
-    }, []);
-
-    const formik = updateModule({
-        initialValues: formatInitialValues(signosVitalesFields),
-        schema: signosVitalesSchema,
-    }, (data) => {
-        saveSessionStorageData("signosVitales", data);
-        saveSignosVitalesHistory(data).then(({ newHistoryRegister }) => {
-            setHistory(prev => {
-                const prevHistory = [...prev];
-                prevHistory.push(newHistoryRegister);
-                saveSessionStorageData("signosVitalesHistory", prevHistory);
-                return prevHistory;
-            });
-        });
     });
 
     return (
@@ -91,7 +79,7 @@ export default function SignosVitales() {
                                 return { headerName: field.name, field: field.property, width: field.name.length * 10 }
                             })
                         }).flat()].flat()}
-                        rows={history}
+                        rows={paciente.signosVitalesHistory || []}
                     />
                 </FullScreenModal>
             </ButtonGroup>
